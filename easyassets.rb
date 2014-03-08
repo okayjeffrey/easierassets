44c714331292ea1fefdbab8a9d754268b05aac94
Jeffrey Larrimore <me@jeffreylarrimore.com>
2014-03-07 17:22:09 -0800

    here is the new one to make it dump the files where they live in a folder Nice and tidy

--- easyassets.rb
+++ easyassets.rb
@@ -64,58 +64,61 @@ if __FILE__==$0
         #      hdpi   | 1.33        1.49625     1.995       2.49375     2.9925
         #     xhdpi   | 2           2.25        3           3.75        4.5
         #    xxhdpi   | 3           3.375       4.5         5.625       6.75
-        
+
         # iOS:
         # iphone1:iphone4:iphone5:ipad:ipad3 ratios are 5:10:10:16:32
         # mdpi:iphone1 ratio is 1:1
-        
+
         scaleRatios = {"drawable-ldpi" => 0.75,
             "drawable-mdpi" => 1.0,
             "drawable-hdpi" => 1.5,
             "drawable-xhdpi" => 2.0,
-            "drawable-xxhdpi" => 3.0,
-            "drawable-sw360dp-ldpi" => 0.84375,
-            "drawable-sw360dp-mdpi" => 1.125,
-            "drawable-sw360dp-hdpi" => 1.6875,
-            "drawable-sw360dp-xhdpi" => 2.25,
-            "drawable-sw360dp-xxhdpi" => 3.375,
-            "drawable-sw480dp-ldpi" => 1.125,
-            "drawable-sw480dp-mdpi" => 1.5,
-            "drawable-sw480dp-hdpi" => 2.25,
-            "drawable-sw480dp-xhdpi" => 3.0,
-            "drawable-sw480dp-xxhdpi" => 4.5,
-            "drawable-sw600dp-ldpi" => 1.40625,
-            "drawable-sw600dp-mdpi" => 1.875,
-            "drawable-sw600dp-hdpi" => 2.8125,
-            "drawable-sw600dp-xhdpi" => 3.75,
-            "drawable-sw600dp-xxhdpi" => 5.625,
-            "drawable-sw720dp-ldpi" => 1.6875,
-            "drawable-sw720dp-mdpi" => 2.25,
-            "drawable-sw720dp-hdpi" => 3.375,
-            "drawable-sw720dp-xhdpi" => 4.5,
-            "drawable-sw720dp-xxhdpi" => 6.75,
-            "iphone1_3g_3gs" => 1.0,
-            "iphone4_4s_5_5s" => 2.0,
-            "ipad1_2_mini" => 3.2,
-            "ipad3_4_air" => 6.4}
-        
+            "drawable-xxhdpi" => 3.0#,
+            # "drawable-sw360dp-ldpi" => 0.84375,
+            # "drawable-sw360dp-mdpi" => 1.125,
+            # "drawable-sw360dp-hdpi" => 1.6875,
+            # "drawable-sw360dp-xhdpi" => 2.25,
+            # "drawable-sw360dp-xxhdpi" => 3.375,
+            # "drawable-sw480dp-ldpi" => 1.125,
+            # "drawable-sw480dp-mdpi" => 1.5,
+            # "drawable-sw480dp-hdpi" => 2.25,
+            # "drawable-sw480dp-xhdpi" => 3.0,
+            # "drawable-sw480dp-xxhdpi" => 4.5,
+            # "drawable-sw600dp-ldpi" => 1.40625,
+            # "drawable-sw600dp-mdpi" => 1.875,
+            # "drawable-sw600dp-hdpi" => 2.8125,
+            # "drawable-sw600dp-xhdpi" => 3.75,
+            # "drawable-sw600dp-xxhdpi" => 5.625,
+            # "drawable-sw720dp-ldpi" => 1.6875,
+            # "drawable-sw720dp-mdpi" => 2.25,
+            # "drawable-sw720dp-hdpi" => 3.375,
+            # "drawable-sw720dp-xhdpi" => 4.5,
+            # "drawable-sw720dp-xxhdpi" => 6.75,
+            # "iphone1_3g_3gs" => 1.0,
+            # "iphone4_4s_5_5s" => 2.0,
+            # "ipad1_2_mini" => 3.2,
+            # "ipad3_4_air" => 6.4
+          }
+
         arguments = ARGV
         arguments.drop(1)
-        
+
         originalWidth = 0
         originalHeight = 0
-        
+
         if ((Integer(ARGV[ARGV.length - 2]) rescue false) && (Integer(ARGV[ARGV.length - 1]) rescue false))
             widthAndHeight = arguments.pop(2)
             originalWidth = widthAndHeight[0].to_f
             originalHeight = widthAndHeight[1].to_f
         end
-        
+
         arguments.each do |originalFileNames|
             Dir[originalFileNames].each do |originalFileName|
+
+                strippedFileName = File.basename(originalFileName, ".*")
                 outputFileName = File.basename(originalFileName, ".*") + ".png"
                 puts "Converting %s to %s\n\n" % [originalFileName, outputFileName]
-                
+
                 if (originalWidth == 0 || originalHeight == 0)
                     command = "identify #{originalFileName}"
                     puts "Run: #{command}\n\n"
@@ -124,33 +127,37 @@ if __FILE__==$0
                     originalWidth = identifyOutput[0].to_f
                     originalHeight = identifyOutput[1].to_f
                 end
-                
+
                 puts "Scaling from width %s and height %s\n\n" % [originalWidth, originalHeight]
-                
+
+                output_dir = "#{File.dirname(originalFileName)}/assets"
+                Dir.mkdir(output_dir)
+
                 scaleRatios.each do |dir, ratio|
                     outputWidth = originalWidth * ratio
                     outputHeight = originalHeight * ratio
                     outputWidth = outputWidth.to_i
                     outputHeight = outputHeight.to_i
                     outputWidthHeight = "#{outputWidth}x#{outputHeight}\\!"
-                    output = "./#{dir}/#{outputFileName}"
-                    
+                    # output = "./#{dir}/#{outputFileName}"
+                    output = "#{output_dir}/#{dir}/#{outputFileName}"
+
                     # Make appropriate directory, if necessary
                     unless File.exists?(dir)
                         puts "Making directory: ./#{dir}/\n"
                         Dir.mkdir(dir)
                     end
-                    
+
                     # Convert svg to png
                     command = "convert -resize #{outputWidthHeight} -density 400 -depth 8 -strip -background none #{originalFileName} #{output}"
                     puts "Run: #{command}\n"
                     `#{command}`
-                    
+
                     # Trim png whitespace
                     command = "convert -trim #{output} #{output}"
                     puts "Run: #{command}\n"
                     `#{command}`
-                    
+
                     # Make filesize much smaller
                     command = "pngquant -f #{output} -o #{output}"
                     puts "Run: #{command}\n\n"
